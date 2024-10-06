2633869b1367a6f231adb6f27f9229dbb5884ece
Hayan Khan <hayankhanmehsood@gmail.com>
2024-10-06 16:53:37 +0500

    ..

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -27,7 +27,16 @@ const Sidebar = () => {
                         const isActive = link.route ===pathname
                         return (
                             <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white':'text-gray-700'}`}>
-                                {link.label}
+                               <Link className="sidebar-link" href={link.route}>
+                               <Image
+                               src={link.icon}
+                                 alt={link.label}
+                                    width={24}
+                                    height={24}
+                                    className={`${isActive && 'brightness-200'}`}
+                               />
+                               {link.label}
+                               </Link>
                             </li>
                         )
                     })
