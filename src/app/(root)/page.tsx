9be1d30f039d0384182df83a126932b339831344
Hayan Khan <hayankhanmehsood@gmail.com>
2024-10-06 15:48:04 +0500

    ...

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,14 @@
+import { UserButton, SignedIn } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+<p>Home</p>
+<SignedIn>
+  <UserButton />
+</SignedIn>
+    </div>
   )
 }
 
