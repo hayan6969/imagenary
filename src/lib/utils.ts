02e87dd3ff8c4e00835c15e7aae77e12770cc227
Hayan Khan <hayankhanmehsood@gmail.com>
2024-10-13 17:51:16 +0500

    ...

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,9 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
-import qs from "qs";
+import qs from "qs"
 import { twMerge } from "tailwind-merge";
-
-import { aspectRatioOptions } from "@/constants";
+import { aspectRatioOptions } from "../../constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
