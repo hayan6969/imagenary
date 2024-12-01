344532ca62fe6de009a43175ddd113eaf386a0f8
Hayan Khan <hayankhanmehsood@gmail.com>
2024-12-01 16:53:21 +0500

    ..

--- src/components/shared/TransformationForm.tsx
+++ src/components/shared/TransformationForm.tsx
@@ -27,6 +27,7 @@ import { CustomField } from "./CustomField"
 import { useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import { updateCredits } from "@/lib/actions/users.actions"
+import MediaUploader from "./MediaUploader"
 
 export const formSchema = z.object({
   Title: z.string(),
@@ -201,6 +202,30 @@ onSelectFieldHandler(value,field.onChange)
         </div>
       )
      }
+
+     <div className="media-uploader-field">
+<CustomField
+
+control={form.control}
+name="publicId"
+className="flex size-full flex-col"
+render={({field})=>(
+  <MediaUploader
+  onValueChange={field.onChange}
+  setImage={setImage}
+  publicId={field.value}
+  image={image}
+  type={type}
+  />
+)
+}
+
+
+
+/>
+
+     </div>
+
      <div className="flex flex-col gap-4">
 
      <Button type="button"
